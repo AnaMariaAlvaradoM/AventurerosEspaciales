2b9d63873b183a27a5048f521619fdbe3146966d
AnaMAriaAlvaradoM <anaa_alvarado@hotmail.com>
2024-10-21 16:56:52 -0500

    Cambio Sprites

--- sprites.py
+++ sprites.py
@@ -16,9 +16,10 @@ class NaveEspacial(pygame.sprite.Sprite):
         if self.rect.right < 800:
             self.rect.move_ip(self.velocidad, 0)
 
-    def disparar(self):
+    def disparar(self, balas, todos_los_sprites):
         bala = Bala(self.rect.centerx, self.rect.top)
-        balas.add((bala,))
+        balas.add(bala)  # Añadir la bala al grupo de balas
+        todos_los_sprites.add(bala)  # Añadir al grupo de todos los sprites
 
 class Enemigo(pygame.sprite.Sprite):
     def __init__(self):
@@ -27,11 +28,11 @@ class Enemigo(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=(random.randrange(0, 735), random.randrange(30, 150)))
         self.velocidad_y = 1
 
-    def mover(self):
-        self.rect.move_ip(0, self.velocidad_y)
-        if self.rect.bottom > 600:
+    def update(self):
+        self.rect.move_ip(0, self.velocidad_y)  # Mover el enemigo hacia abajo
+        if self.rect.bottom > 600:  # Si el enemigo sale de la pantalla
             self.rect.top = 30
-            self.rect.x = random.randrange(0, 735)
+            self.rect.x = random.randrange(0, 735)  # Reposicionar el enemigo
 
 class Bala(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -39,18 +40,8 @@ class Bala(pygame.sprite.Sprite):
         self.image = pygame.image.load("bala.png")
         self.rect = self.image.get_rect(center=(x, y))
         self.velocidad_y = -10
-    def actualizar(self):
-        self.rect.move_ip(0, self.velocidad_y)
+
+    def update(self):
+        self.rect.move_ip(0, self.velocidad_y)  # Mover la bala hacia arriba
         if self.rect.top < 0:
-            self.kill()
-
-todos_los_sprites = pygame.sprite.Group()            
-balas = pygame.sprite.Group()           
-enemigos = pygame.sprite.Group()
-def crear_enemigo():
-    nuevo_enemigo = Enemigo()
-    enemigos.add(nuevo_enemigo)
-
-def crear_enemigos():
-    for _ in range(10):
-        crear_enemigo()
+            self.kill()  # Eliminar la bala si sale de la pantalla
